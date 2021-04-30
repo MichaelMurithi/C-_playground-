7596b3baf25aacadccb312da2ff0095c405d2ee2
Myke <mykemitrichev@gmail.com>
2021-04-30 20:41:23 +0200

    Implemented interfaces

--- SchoolApp/ScienceExperiment.cs
+++ SchoolApp/ScienceExperiment.cs
@@ -4,12 +4,13 @@ using System.Text;
 
 namespace SchoolLibrary
 {
-	public class ScienceExperiment
+	public class ScienceExperiment : IScored
 	{
 		public string Hypothesis { get; set; }
 		public string Materials { get; set; }
 		public string Method { get; set; }
 		public string Conclusion { get; set; }
-
+		public float Score { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public float MaximumScore { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 	}
 }
