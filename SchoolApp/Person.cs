7adde8d99fc5bc2658e593aa32cca3bc71a48cc0
Myke <mykemitrichev@gmail.com>
2021-04-30 18:40:51 +0200

    Practiced Abstract methods implementation

---
+++ SchoolApp/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+	//The Person class is abstract to prevent it from being instantiated directly
+	public abstract class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+
+		public abstract float ComputeGradeAverage();
+
+	}
+}
---
+++ SchoolApp/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+	class Teacher : Person
+	{
+		
+		public string Subject { get; set; }
+
+		public override float ComputeGradeAverage()
+		{
+			//TODO: Fix the Implementation Later
+			return 4.0f;
+		}
+	}
+}
