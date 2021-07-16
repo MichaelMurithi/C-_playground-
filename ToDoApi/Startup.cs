cb3347614cc0aaa0ad8327900c211e2067eecf20
Michael Murithi <mykemitrichev@gmail.com>
2021-07-16 23:48:39 +0200

    feat(toDoapi): Created controllers and data access for toido app API

--- ToDoApi/Startup.cs
+++ ToDoApi/Startup.cs
@@ -1,16 +1,11 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ToDoApi.Models;
 
 namespace ToDoApi
 {
@@ -32,6 +27,7 @@ namespace ToDoApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ToDoApi", Version = "v1" });
             });
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
