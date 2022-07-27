4a452f221d139c3aa89e37120a48546f014baf36
Michael Kariuki <michael.kariuki@siemens-healthineers.com>
2022-07-27 19:41:14 +0200

    practice(csharp-records): Added app to learn about records in c#

---
+++ ClassesAndRecords/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    /// <summary>
+    /// - A record instance is immutable while class instance' properties can be modified
+    /// - Immutability: Objects that should'nt change should'nt be changeable
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        var ubuntuEvent = new Event("Ubuntu", "Zelezna Studenka");
+        var events = new EventRecord("Kenyan BBQ", "Lanfranconi");
+    }
+
+    public class Event
+    {
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+
+        public Event(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
+    }
+
+    public record EventRecord(string? name, string? location);
+}
