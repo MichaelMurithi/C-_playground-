a59bd952089ce8f2dafdc4ca301eb81afbbb9ebd
Michael Kariuki <michael.kariuki@siemens-healthineers.com>
2022-07-31 08:39:58 +0200

    learn(ExtensionMethods): Setup projects to learn extension methods

---
+++ ExtensionMethods/ExtensionMethods.Library/ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ExtensionMethods.Library
+{
+    public static class ConfigurationExtensions
+    {
+        public static bool IsLoaded(this IConfiguration config)
+        {
+            return config != null && config.AsEnumerable().Any();
+        }
+
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods.Tests/ConfigurationTests.cs
@@ -0,0 +1,17 @@
+﻿using ExtensionMethods.Library;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace ExtensionMethods.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            IConfiguration config = null;
+
+            Assert.IsFalse(config.IsLoaded());
+        }
+    }
+}
