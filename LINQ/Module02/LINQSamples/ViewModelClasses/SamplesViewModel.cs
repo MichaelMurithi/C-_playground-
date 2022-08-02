8121b286166be0a2844ffd834a21d2f8c50e5070
Michael Kariuki <michael.kariuki@siemens-healthineers.com>
2022-08-02 21:13:29 +0200

    practice(LINQ): practiced use of anonymous classes

--- LINQ/Module02/LINQSamples/ViewModelClasses/SamplesViewModel.cs
+++ LINQ/Module02/LINQSamples/ViewModelClasses/SamplesViewModel.cs
@@ -134,24 +134,37 @@ namespace LINQSamples
             if (UseQuerySyntax)
             {
                 // Query Syntax
-
+                var products = (from prod in Products
+                    select new
+                    {
+                        Identifier = prod.ProductID,
+                        ProductName = prod.Name,
+                        ProductSize = prod.Size
+                    });
                 // Loop through anonymous class
-                //foreach (var prod in products) {
-                //  sb.AppendLine($"Product ID: {prod.Identifier}");
-                //  sb.AppendLine($"   Product Name: {prod.ProductName}");
-                //  sb.AppendLine($"   Product Size: {prod.ProductSize}");
-                //}
+                foreach (var prod in products)
+                {
+                    sb.AppendLine($"Product ID: {prod.Identifier}");
+                    sb.AppendLine($"   Product Name: {prod.ProductName}");
+                    sb.AppendLine($"   Product Size: {prod.ProductSize}");
+                }
             }
             else
             {
                 // Method Syntax
-
+                var products = Products.Select(prod => new
+                {
+                    Identifier = prod.ProductID,
+                    ProductName = prod.Name,
+                    ProductSize = prod.Size
+                });
                 // Loop through anonymous class
-                //foreach (var prod in products) {
-                //  sb.AppendLine($"Product ID: {prod.Identifier}");
-                //  sb.AppendLine($"   Product Name: {prod.ProductName}");
-                //  sb.AppendLine($"   Product Size: {prod.ProductSize}");
-                //}
+                foreach (var prod in products)
+                {
+                    sb.AppendLine($"Product ID: {prod.Identifier}");
+                    sb.AppendLine($"   Product Name: {prod.ProductName}");
+                    sb.AppendLine($"   Product Size: {prod.ProductSize}");
+                }
             }
 
             ResultText = sb.ToString();
